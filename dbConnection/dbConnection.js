d570536a70eb830c385f7343dd4c6be191a5bdb9
gopi12 <gopi1.dhara@gmail.com>
2021-06-05 20:07:29 +0530

    check modification db connection

--- dbConnection/dbConnection.js
+++ dbConnection/dbConnection.js
@@ -1,12 +1,12 @@
 //momgo db connection
 const mongoose = require('mongoose')
-let db='mongodb://gopi:12345678@localhost:27017/videoDB';  
-//let db ="mongodb://u0qchhlcveatq3tfihhz:vBIGEph0gYSCuWC35aFO@be3hukrrvu9sgbd-mongodb.services.clever-cloud.com:27017/be3hukrrvu9sgbd"
+//let db='mongodb://gopi:12345678@localhost:27017/videoDB';  
+let db='mongodb://uby51yxcyvnfnhq95eun:PFnzSKEOPIt2ZH6siuOe@b1xddzmd3opgti1-mongodb.services.clever-cloud.com:27017/b1xddzmd3opgti1';
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology:true },function(err){
     if(err){
         throw err;
     }else{ 
-        console.log("Mongo DB Connected Successfully");  
+        console.log("Mongo DB Live Connected Successfully");  
     }
 }) 
 
